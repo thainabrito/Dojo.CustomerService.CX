e2c4516206077ff99d6a9935604bb6b0fd4097f5
maria <thainastefannia@gmail.com>
2022-12-07 09:11:39 -0300

    cria rotas

--- Dojo.CustomerService.CX/Controllers/CsatController.cs
+++ Dojo.CustomerService.CX/Controllers/CsatController.cs
@@ -10,14 +10,6 @@ namespace Dojo.CustomerService.CX.Controllers
     [Route("/")]
     public class CsatController : ControllerBase
     {
-        [HttpGet("csat")]
-        public async Task<ActionResult> Index()
-        {
-            var csatMongo = new CSATMongodb();
-            var lista = await csatMongo.Todos();
-            return StatusCode(200, lista);
-        }
-
         [HttpPost("csat")]
         public async Task<ActionResult> Create([FromBody] Csat csat)
         {
@@ -59,14 +51,92 @@ namespace Dojo.CustomerService.CX.Controllers
                 return StatusCode(400, new { mensagem = "id inválido" });
             }
 
-            var buscaId = new CSATMongodb();
+            var csatMongoDB = new CSATMongodb();
 
-            var idReturn = buscaId.BuscaPorId(guidOutput);
+            var idReturn = csatMongoDB.BuscaPorId(guidOutput);
             if (idReturn.Result == null)
             {
-                return StatusCode(400, new { mensagem = "Não foi encontrado o csat" });
+                return StatusCode(404, new { mensagem = "Não foi encontrado o csat" });
             }
             return StatusCode(200, idReturn.Result);
+        }
+
+        [HttpPut("csat/comment/{id}")]
+        public async Task<ActionResult> Atualizar([FromRoute] string id, [FromBody] string comment)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return StatusCode(400, new { mensagem = "Id não pode estar vazio" });
+            }
+            var guidOutput = new Guid();
+            bool isValid = Guid.TryParse(id, out guidOutput);
+            if (!isValid)
+            {
+                return StatusCode(400, new { mensagem = "id inválido" });
+            }
+
+            var csatMongoDB = new CSATMongodb();
+
+            var idReturn = csatMongoDB.BuscaPorId(guidOutput);
+            if (idReturn.Result == null)
+            {
+                return StatusCode(404, new { mensagem = "Não foi encontrado o csat" });
+            }
+            var csat = idReturn.Result;
+            csat.Comment = comment;
+            csatMongoDB.Atualizar(csat);
+            return StatusCode(204);
+        }
+
+        [HttpPut("csat/problemSolved/{id}")]
+        public async Task<ActionResult> Atualizar([FromRoute] string id, [FromBody] bool problemSolved)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return StatusCode(400, new { mensagem = "Id não pode estar vazio" });
+            }
+            var guidOutput = new Guid();
+            bool isValid = Guid.TryParse(id, out guidOutput);
+            if (!isValid)
+            {
+                return StatusCode(400, new { mensagem = "id inválido" });
+            }
+
+            var csatMongoDB = new CSATMongodb();
+
+            var idReturn = csatMongoDB.BuscaPorId(guidOutput);
+            if (idReturn.Result == null)
+            {
+                return StatusCode(404, new { mensagem = "Não foi encontrado o csat" });
+            }
+            var csat = idReturn.Result;
+            csat.ProblemSolved = problemSolved;
+            csatMongoDB.Atualizar(csat);
+            return StatusCode(204);
+        }
+
+        [HttpGet("csat/listar/{score?}/{problemSolved?}/{attendantEmail?}")]
+        public async Task<ActionResult> Index([FromRoute] int? score = null, [FromRoute] bool? problemSolved = null, [FromRoute] string? attendantEmail = "")
+        {
+            var csatMongo = new CSATMongodb();
+            var lista = await csatMongo.Todos();
+
+            if (score >= 1 && score <= 5)
+            {
+                lista = lista.Where(s => s.Score == score).ToList();
+            }
+
+            if (problemSolved != null)
+            {
+                lista = lista.Where(s => s.ProblemSolved == problemSolved).ToList();
+            }
+
+            if (attendantEmail != "")
+            {
+                lista = lista.Where(s => s.AttendantEmail == attendantEmail).ToList();
+            }
+
+            return StatusCode(200, lista);
         }
     }
 }
